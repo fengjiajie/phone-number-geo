0e9a2da61fc83408fd6efc693da8edddb93996e6
Feng Jiajie <fengjiajie@sensorsdata.cn>
2018-10-30 19:11:20 +0800

    更新 2018.10 库

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 ## 手机号码归属地本地解析(Java)
 
-参照 lovedboy/phone 实现的 Java 版本。原库以及其他语言版本地址：
-https://github.com/lovedboy/phone
+参照 ls0f/phone 实现的 Java 版本。原库以及其他语言版本地址：
 
-非常感谢原作者提供号码归属地库。
+* python: https://github.com/ls0f/phone
+* go: https://github.com/xluohome/phonedata
+
+2018.10.30 升级到了 xluohome 提供的最新版库。
+
+非常感谢 ls0f、xluohome 提供号码归属地库。
 
 如该代码或库文件侵犯了您的权益，请联系我删除，谢谢。
 
@@ -12,6 +16,8 @@ https://github.com/lovedboy/phone
 ```java
 PhoneNumberGeo phoneNumberGeo = new PhoneNumberGeo();
 PhoneNumberInfo phoneNumberInfo = phoneNumberGeo.lookup("13006150000");
+
+// PhoneNumberInfo{phoneNumber='13006150000', province='湖北', city='武汉', zipCode='430000', areaCode='027', phoneType='联通'}
 ```
 
 ## License
--- src/main/java/cn/kbyte/utils/phone/PhoneNumberGeo.java
+++ src/main/java/cn/kbyte/utils/phone/PhoneNumberGeo.java
@@ -12,51 +12,53 @@ import java.util.Scanner;
  */
 public class PhoneNumberGeo {
 
-  private static String[] numberType = {null, "移动", "联通", "电信", "电信虚拟运营商", "联通虚拟运营商", "移动虚拟运营商"};
+  private static final String[] PHONE_NUMBER_TYPE = {null, "移动", "联通", "电信", "电信虚拟运营商", "联通虚拟运营商", "移动虚拟运营商"};
   private static final int INDEX_SEGMENT_LENGTH = 9;
+  private static final int DATA_FILE_LENGTH_HINT = 3747505;
+  private static final String PHONE_DAT_FILE_PATH = "phone-number-geo/phone.dat";
 
   private static byte[] dataByteArray;
+  private static int indexAreaOffset;
+  private static int phoneRecordCount;
+
   private ByteBuffer byteBuffer;
-  private int indexAreaOffset = -1;
-  private int phoneRecordCount = -1;
+
+  private static synchronized void initData() {
+    if (dataByteArray == null) {
+      ByteArrayOutputStream byteData = new ByteArrayOutputStream(DATA_FILE_LENGTH_HINT);
+      byte[] buffer = new byte[1024];
+
+      int readBytesLength;
+      try (InputStream inputStream = PhoneNumberGeo.class.getClassLoader().getResourceAsStream(PHONE_DAT_FILE_PATH)) {
+        while ((readBytesLength = inputStream.read(buffer)) != -1) {
+          byteData.write(buffer, 0, readBytesLength);
+        }
+      } catch (Exception e) {
+        System.err.println("Can't find phone.dat in classpath: " + PHONE_DAT_FILE_PATH);
+        e.printStackTrace();
+        throw new RuntimeException(e);
+      }
+
+      dataByteArray = byteData.toByteArray();
+
+      ByteBuffer byteBuffer = ByteBuffer.wrap(dataByteArray);
+      byteBuffer.order(ByteOrder.LITTLE_ENDIAN);
+      int dataVersion = byteBuffer.getInt();
+      indexAreaOffset = byteBuffer.getInt();
+      phoneRecordCount = (dataByteArray.length - indexAreaOffset) / INDEX_SEGMENT_LENGTH;
+
+      // print data version
+      // System.out.println(dataVersion);
+      // print record count
+      // System.out.println(phoneRecordCount);
+    }
+  }
 
   public PhoneNumberGeo() {
-    if (dataByteArray == null) {
-      synchronized (PhoneNumberGeo.class) {
-        if (dataByteArray == null) {
-          ByteArrayOutputStream byteData = new ByteArrayOutputStream();
-          byte[] buffer = new byte[1024];
-
-          int readBytesLength;
-          try {
-            InputStream inputStream = PhoneNumberGeo.class.getClassLoader()
-                .getResourceAsStream("phone-number-geo/phone.dat");
-            while ((readBytesLength = inputStream.read(buffer)) != -1) {
-              byteData.write(buffer, 0, readBytesLength);
-            }
-            inputStream.close();
-          } catch (Exception e) {
-            System.err.println("Can't find phone.dat in classpath:phone-number-geo/phone.dat");
-            e.printStackTrace();
-            throw new RuntimeException(e);
-          }
-
-          dataByteArray = byteData.toByteArray();
-        }
-      }
-    }
+    initData();
 
     byteBuffer = ByteBuffer.wrap(dataByteArray);
     byteBuffer.order(ByteOrder.LITTLE_ENDIAN);
-    int dataVersion = byteBuffer.getInt();
-    indexAreaOffset = byteBuffer.getInt();
-
-    // print data version
-    // System.out.println(dataVersion);
-
-    phoneRecordCount = (dataByteArray.length - indexAreaOffset) / INDEX_SEGMENT_LENGTH;
-    // print record count
-    // System.out.println(phoneRecordCount);
   }
 
   public PhoneNumberInfo lookup(String phoneNumber) {
@@ -96,8 +98,7 @@ public class PhoneNumberGeo {
           }
         }
 
-        String infoString =
-            new String(dataByteArray, infoBeginOffset, infoLength, StandardCharsets.UTF_8);
+        String infoString = new String(dataByteArray, infoBeginOffset, infoLength, StandardCharsets.UTF_8);
         String[] infoSegments = infoString.split("\\|");
 
         PhoneNumberInfo phoneNumberInfo = new PhoneNumberInfo();
@@ -106,7 +107,7 @@ public class PhoneNumberGeo {
         phoneNumberInfo.setCity(infoSegments[1]);
         phoneNumberInfo.setZipCode(infoSegments[2]);
         phoneNumberInfo.setAreaCode(infoSegments[3]);
-        phoneNumberInfo.setPhoneType(numberType[phoneType]);
+        phoneNumberInfo.setPhoneType(PHONE_NUMBER_TYPE[phoneType]);
         return phoneNumberInfo;
       }
     }
